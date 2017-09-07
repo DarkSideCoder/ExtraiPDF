4de2b0f154a0468231d16ecac8a256a80ad38522
felipe2rone <felipe2rone@gmail.com>
2017-09-07 14:26:26 -0300

    Extrai

---
+++ ExtraiPDF/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace ExtraiPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btAction = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lcLines = new System.Windows.Forms.Label();
+            this.rtbResult = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btAction
+            // 
+            this.btAction.Location = new System.Drawing.Point(554, 14);
+            this.btAction.Name = "btAction";
+            this.btAction.Size = new System.Drawing.Size(75, 23);
+            this.btAction.TabIndex = 0;
+            this.btAction.Text = "GO";
+            this.btAction.UseVisualStyleBackColor = true;
+            this.btAction.Click += new System.EventHandler(this.btAction_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 458);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Lines:";
+            // 
+            // lcLines
+            // 
+            this.lcLines.AutoSize = true;
+            this.lcLines.Location = new System.Drawing.Point(62, 458);
+            this.lcLines.Name = "lcLines";
+            this.lcLines.Size = new System.Drawing.Size(10, 13);
+            this.lcLines.TabIndex = 3;
+            this.lcLines.Text = "-";
+            // 
+            // rtbResult
+            // 
+            this.rtbResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbResult.Location = new System.Drawing.Point(12, 65);
+            this.rtbResult.Name = "rtbResult";
+            this.rtbResult.Size = new System.Drawing.Size(616, 382);
+            this.rtbResult.TabIndex = 4;
+            this.rtbResult.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(554, 453);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(454, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Path:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 46);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Choose HTML File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(91, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Protocol";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(143, 46);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(55, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "HTTP";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(204, 46);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(62, 17);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "HTTPS";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(326, 45);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(47, 17);
+            this.checkBox3.TabIndex = 12;
+            this.checkBox3.Text = "PDF";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(379, 45);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(48, 17);
+            this.checkBox4.TabIndex = 13;
+            this.checkBox4.Text = "MP3";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(433, 45);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(48, 17);
+            this.checkBox5.TabIndex = 14;
+            this.checkBox5.Text = "MP4";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(487, 45);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(61, 17);
+            this.checkBox6.TabIndex = 15;
+            this.checkBox6.Text = "Custom";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(555, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(74, 20);
+            this.textBox2.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(273, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Extension";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 488);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rtbResult);
+            this.Controls.Add(this.lcLines);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btAction);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btAction;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lcLines;
+        private System.Windows.Forms.RichTextBox rtbResult;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ ExtraiPDF/Form1.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExtraiPDF
+{
+    public partial class Form1 : Form
+    {
+        String htmlFile = @"c:\windows\temp\enem.html";        
+        String HTTP = "http://";
+        String HTTPS = "*https://";
+        String HREF = "href=";
+        String PDF = ".pdf";
+        String MP3 = "*.mp3";
+        String MP4 = "*.mp4";
+        String Custom = textBox2.text;    
+      
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region Botões
+        private void btAction_Click(object sender, EventArgs e)
+        {
+            SearchPDF();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+        #endregion
+        #region Funções
+        public void SearchPDF()
+        {
+            #region Variaveis
+            String content;
+            String line;
+            String result1;
+            String result2;
+            int cLine = 0;            
+            int stLine = 0;            
+            int fiLine = 0;           
+            int szLine = 0;
+            int l2Line = 0;        
+            StreamReader str = new StreamReader(htmlFile);
+            #endregion
+            #region Leitura do HTML
+            while ((content = str.ReadLine()) != null)
+            {
+                cLine++; //contando as linhas
+                line = content; //recebendo o conteudo do html linha a linha
+                #region Contem / Não Contem
+                if (line.Contains(HTTP) && line.Contains(PDF) && line.Contains(HREF)) //verificando se a linha contem o http
+                {
+                    #region Primeira Verificação
+                    result1 = line;
+                    stLine = line.IndexOf(HREF);                                     
+                    result1 = line.Substring(stLine);
+                    l2Line = line.IndexOf(PDF);
+                    fiLine = l2Line + 4;
+                    szLine = fiLine - stLine;
+                    result1 = line.Substring(stLine, szLine);
+                    #endregion
+
+                    #region Segunda Verificação
+                    result2 = result1;                  
+                    stLine = result1.LastIndexOf(HREF);
+                    result2 = result1.Substring(stLine);
+                    l2Line = result1.IndexOf(PDF);
+                    fiLine = l2Line + 4;
+                    szLine = fiLine - stLine;
+                    result2 = result1.Substring(stLine, szLine);
+                    #endregion
+
+                    #region Resultado
+                    line = result2;
+                    rtbResult.AppendText(cLine+" | "+line);                   
+                    rtbResult.AppendText("\n");
+                    #endregion
+
+        public void SearchMP3()
+        {
+            #region Variaveis
+            String content;
+            String line;
+            String result1;
+            String result2;
+            int cLine = 0;
+            int stLine = 0;
+            int fiLine = 0;
+            int szLine = 0;
+            int l2Line = 0;
+            StreamReader str = new StreamReader(htmlFile);
+            #endregion
+            #region Leitura do HTML
+            while ((content = str.ReadLine()) != null)
+            {
+                cLine++; //contando as linhas
+                line = content; //recebendo o conteudo do html linha a linha
+                #region Contem / Não Contem
+                if (line.Contains(HTTP) && line.Contains(MP3) && line.Contains(HREF)) //verificando se a linha contem o http
+                {
+                    #region Primeira Verificação
+                    result1 = line;
+                    stLine = line.IndexOf(HREF);
+                    result1 = line.Substring(stLine);
+                    l2Line = line.IndexOf(MP3);
+                    fiLine = l2Line + 4;
+                    szLine = fiLine - stLine;
+                    result1 = line.Substring(stLine, szLine);
+                    #endregion
+
+                    #region Segunda Verificação
+                    result2 = result1;
+                    stLine = result1.LastIndexOf(HREF);
+                    result2 = result1.Substring(stLine);
+                    l2Line = result1.IndexOf(MP3);
+                    fiLine = l2Line + 4;
+                    szLine = fiLine - stLine;
+                    result2 = result1.Substring(stLine, szLine);
+                    #endregion
+
+                    #region Resultado
+                    line = result2;
+                    rtbResult.AppendText(cLine + " | " + line);
+                    rtbResult.AppendText("\n");
+                    #endregion
+                }
+                 public void SearcMP4()
+        {
+            #region Variaveis
+            String content;
+            String line;
+            String result1;
+            String result2;
+            int cLine = 0;
+            int stLine = 0;
+            int fiLine = 0;
+            int szLine = 0;
+            int l2Line = 0;
+            StreamReader str = new StreamReader(htmlFile);
+            #endregion
+            #region Leitura do HTML
+            while ((content = str.ReadLine()) != null)
+            {
+                cLine++; //contando as linhas
+                line = content; //recebendo o conteudo do html linha a linha
+                #region Contem / Não Contem
+                if (line.Contains(HTTP) && line.Contains(MP4) && line.Contains(HREF)) //verificando se a linha contem o http
+                {
+                    #region Primeira Verificação
+                    result1 = line;
+                    stLine = line.IndexOf(HREF);
+                    result1 = line.Substring(stLine);
+                    l2Line = line.IndexOf(MP4);
+                    fiLine = l2Line + 4;
+                    szLine = fiLine - stLine;
+                    result1 = line.Substring(stLine, szLine);
+                    #endregion
+
+                    #region Segunda Verificação
+                    result2 = result1;
+                    stLine = result1.LastIndexOf(HREF);
+                    result2 = result1.Substring(stLine);
+                    l2Line = result1.IndexOf(MP4);
+                    fiLine = l2Line + 4;
+                    szLine = fiLine - stLine;
+                    result2 = result1.Substring(stLine, szLine);
+                    #endregion
+
+                    #region Resultado
+                    line = result2;
+                    rtbResult.AppendText(cLine + " | " + line);
+                    rtbResult.AppendText("\n");
+                    #endregion
+                }
+                 public void SearchCustom()
+        {
+            #region Variaveis
+            String content;
+            String line;
+            String result1;
+            String result2;
+            int cLine = 0;
+            int stLine = 0;
+            int fiLine = 0;
+            int szLine = 0;
+            int l2Line = 0;
+            StreamReader str = new StreamReader(htmlFile);
+            #endregion
+            #region Leitura do HTML
+            while ((content = str.ReadLine()) != null)
+            {
+                cLine++; //contando as linhas
+                line = content; //recebendo o conteudo do html linha a linha
+                #region Contem / Não Contem
+                if (line.Contains(HTTP) && line.Contains(Custom) && line.Contains(HREF)) //verificando se a linha contem o http
+                {
+                    #region Primeira Verificação
+                    result1 = line;
+                    stLine = line.IndexOf(HREF);
+                    result1 = line.Substring(stLine);
+                    l2Line = line.IndexOf(Custom);
+                    fiLine = l2Line + 4;
+                    szLine = fiLine - stLine;
+                    result1 = line.Substring(stLine, szLine);
+                    #endregion
+
+                    #region Segunda Verificação
+                    result2 = result1;
+                    stLine = result1.LastIndexOf(HREF);
+                    result2 = result1.Substring(stLine);
+                    l2Line = result1.IndexOf(Custom);
+                    fiLine = l2Line + 4;
+                    szLine = fiLine - stLine;
+                    result2 = result1.Substring(stLine, szLine);
+                    #endregion
+
+                    #region Resultado
+                    line = result2;
+                    rtbResult.AppendText(cLine + " | " + line);
+                    rtbResult.AppendText("\n");
+                    #endregion
+                }
+                 
+
+                #endregion
+            }
+            #endregion
+
+            lcLines.Text = Convert.ToString(cLine);
+        }
+        public void Clear()
+        {
+            rtbResult.Text = " ";
+        }
+        public void Open()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            ofd.Title = "Selecionar Arquivo";
+            ofd.InitialDirectory = @"c:\windows\temp";
+            ofd.Filter = "HTML|*.html";
+            ofd.ShowDialog();
+
+        }
+        public void Checks()
+        {
+            if (checkBox1.Checked)
+            {
+                checkBox1.Text = "Checked";
+            }
+            else
+            {
+                checkBox1.Text = "Unchecked";
+            }
+            if (checkBox2.Checked)
+            {
+                checkBox2.Text = "Checked";
+            }
+            else
+            {
+                checkBox2.Text = "Unchecked";
+            }
+            if (checkBox3.Checked)
+            {
+                checkBox3.Text = "Checked";
+            }
+            else
+            {
+                checkBox3.Text = "Unchecked";
+            }
+            if (checkBox4.Checked)
+            {
+                checkBox4.Text = "Checked";
+            }
+            else
+            {
+                checkBox4.Text = "Unchecked";
+            }
+            if (checkBox5.Checked)
+            {
+                checkBox5.Text = "Checked";
+            }
+            else
+            {
+                checkBox5.Text = "Unchecked";
+            }
+            if (checkBox6.Checked)
+            {
+                checkBox6.Text = "Checked";
+            }
+            else
+            {
+                checkBox6.Text = "Unchecked";
+            }
+
+        }
+        #endregion
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Open();
+        }
+    }
+}
