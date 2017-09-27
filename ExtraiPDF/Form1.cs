435ad004a3fced40f779ddcf345fb5d40b8b23f1
DarkSideCoder <31574210+DarkSideCoder@users.noreply.github.com>
2017-09-26 21:06:46 -0300

    Update Form1.cs

--- ExtraiPDF/Form1.cs
+++ ExtraiPDF/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,25 +13,86 @@ namespace ExtraiPDF
 {
     public partial class Form1 : Form
     {
-        String htmlFile = @"c:\windows\temp\enem.html";        
-        String HTTP = "http://";
-        String HTTPS = "*https://";
-        String HREF = "href=";
-        String PDF = ".pdf";
-        String MP3 = "*.mp3";
-        String MP4 = "*.mp4";
-        String Custom = textBox2.text;    
-      
-
-
         public Form1()
         {
             InitializeComponent();
         }
+        String htmlFile = null;
         #region Botões
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Open();
+        }
         private void btAction_Click(object sender, EventArgs e)
         {
-            SearchPDF();
+            String HTTP = "http://";
+            String HTTPS = "*https://";
+            String PDF = ".pdf";
+            String MP3 = "*.mp3";
+            String MP4 = "*.mp4";
+            String CST = textBox2.Text;
+            CST = CST.ToLower();
+            CST = "." + CST;
+            if (this.PDF.Checked)
+            {
+                if (this.HTTP.Checked)
+                {
+                    Search(PDF, HTTP);
+                }
+                if (this.HTTPS.Checked)
+                {
+                    Search(PDF, HTTPS);
+                }
+                else
+                {
+                    Search(PDF, HTTP);
+                }            
+            }
+            if (this.MP3.Checked)
+            {
+                if (this.HTTP.Checked)
+                {
+                    Search(MP3, HTTP);
+                }
+                if (this.HTTPS.Checked)
+                {
+                    Search(MP3, HTTPS);
+                }
+                else
+                {
+                    Search(MP3, HTTP);
+                }
+            }
+            if (this.MP4.Checked)
+            {
+                if (this.HTTP.Checked)
+                {
+                    Search(MP4, HTTP);
+                }
+                if (this.HTTPS.Checked)
+                {
+                    Search(MP4, HTTPS);
+                }
+                else
+                {
+                    Search(MP4, HTTP);
+                }
+            }
+            if (Custom.Checked)
+            {
+                if (this.HTTP.Checked)
+                {
+                    Search(CST, HTTP);
+                }
+                if (this.HTTPS.Checked)
+                {
+                    Search(CST, HTTPS);
+                }
+                else
+                {
+                    Search(CST, HTTP);
+                }
+            }
         }
         private void button1_Click(object sender, EventArgs e)
         {
@@ -39,206 +100,69 @@ namespace ExtraiPDF
         }
         #endregion
         #region Funções
-        public void SearchPDF()
+        public void Search(String EXT, String TYPE)
         {
-            #region Variaveis
-            String content;
-            String line;
-            String result1;
-            String result2;
-            int cLine = 0;            
-            int stLine = 0;            
-            int fiLine = 0;           
-            int szLine = 0;
-            int l2Line = 0;        
-            StreamReader str = new StreamReader(htmlFile);
-            #endregion
-            #region Leitura do HTML
-            while ((content = str.ReadLine()) != null)
+            if(htmlFile == null)
             {
-                cLine++; //contando as linhas
-                line = content; //recebendo o conteudo do html linha a linha
-                #region Contem / Não Contem
-                if (line.Contains(HTTP) && line.Contains(PDF) && line.Contains(HREF)) //verificando se a linha contem o http
-                {
-                    #region Primeira Verificação
-                    result1 = line;
-                    stLine = line.IndexOf(HREF);                                     
-                    result1 = line.Substring(stLine);
-                    l2Line = line.IndexOf(PDF);
-                    fiLine = l2Line + 4;
-                    szLine = fiLine - stLine;
-                    result1 = line.Substring(stLine, szLine);
-                    #endregion
-
-                    #region Segunda Verificação
-                    result2 = result1;                  
-                    stLine = result1.LastIndexOf(HREF);
-                    result2 = result1.Substring(stLine);
-                    l2Line = result1.IndexOf(PDF);
-                    fiLine = l2Line + 4;
-                    szLine = fiLine - stLine;
-                    result2 = result1.Substring(stLine, szLine);
-                    #endregion
-
-                    #region Resultado
-                    line = result2;
-                    rtbResult.AppendText(cLine+" | "+line);                   
-                    rtbResult.AppendText("\n");
-                    #endregion
-
-        public void SearchMP3()
-        {
-            #region Variaveis
-            String content;
-            String line;
-            String result1;
-            String result2;
-            int cLine = 0;
-            int stLine = 0;
-            int fiLine = 0;
-            int szLine = 0;
-            int l2Line = 0;
-            StreamReader str = new StreamReader(htmlFile);
-            #endregion
-            #region Leitura do HTML
-            while ((content = str.ReadLine()) != null)
-            {
-                cLine++; //contando as linhas
-                line = content; //recebendo o conteudo do html linha a linha
-                #region Contem / Não Contem
-                if (line.Contains(HTTP) && line.Contains(MP3) && line.Contains(HREF)) //verificando se a linha contem o http
-                {
-                    #region Primeira Verificação
-                    result1 = line;
-                    stLine = line.IndexOf(HREF);
-                    result1 = line.Substring(stLine);
-                    l2Line = line.IndexOf(MP3);
-                    fiLine = l2Line + 4;
-                    szLine = fiLine - stLine;
-                    result1 = line.Substring(stLine, szLine);
-                    #endregion
-
-                    #region Segunda Verificação
-                    result2 = result1;
-                    stLine = result1.LastIndexOf(HREF);
-                    result2 = result1.Substring(stLine);
-                    l2Line = result1.IndexOf(MP3);
-                    fiLine = l2Line + 4;
-                    szLine = fiLine - stLine;
-                    result2 = result1.Substring(stLine, szLine);
-                    #endregion
-
-                    #region Resultado
-                    line = result2;
-                    rtbResult.AppendText(cLine + " | " + line);
-                    rtbResult.AppendText("\n");
-                    #endregion
-                }
-                 public void SearcMP4()
-        {
-            #region Variaveis
-            String content;
-            String line;
-            String result1;
-            String result2;
-            int cLine = 0;
-            int stLine = 0;
-            int fiLine = 0;
-            int szLine = 0;
-            int l2Line = 0;
-            StreamReader str = new StreamReader(htmlFile);
-            #endregion
-            #region Leitura do HTML
-            while ((content = str.ReadLine()) != null)
-            {
-                cLine++; //contando as linhas
-                line = content; //recebendo o conteudo do html linha a linha
-                #region Contem / Não Contem
-                if (line.Contains(HTTP) && line.Contains(MP4) && line.Contains(HREF)) //verificando se a linha contem o http
-                {
-                    #region Primeira Verificação
-                    result1 = line;
-                    stLine = line.IndexOf(HREF);
-                    result1 = line.Substring(stLine);
-                    l2Line = line.IndexOf(MP4);
-                    fiLine = l2Line + 4;
-                    szLine = fiLine - stLine;
-                    result1 = line.Substring(stLine, szLine);
-                    #endregion
-
-                    #region Segunda Verificação
-                    result2 = result1;
-                    stLine = result1.LastIndexOf(HREF);
-                    result2 = result1.Substring(stLine);
-                    l2Line = result1.IndexOf(MP4);
-                    fiLine = l2Line + 4;
-                    szLine = fiLine - stLine;
-                    result2 = result1.Substring(stLine, szLine);
-                    #endregion
-
-                    #region Resultado
-                    line = result2;
-                    rtbResult.AppendText(cLine + " | " + line);
-                    rtbResult.AppendText("\n");
-                    #endregion
-                }
-                 public void SearchCustom()
-        {
-            #region Variaveis
-            String content;
-            String line;
-            String result1;
-            String result2;
-            int cLine = 0;
-            int stLine = 0;
-            int fiLine = 0;
-            int szLine = 0;
-            int l2Line = 0;
-            StreamReader str = new StreamReader(htmlFile);
-            #endregion
-            #region Leitura do HTML
-            while ((content = str.ReadLine()) != null)
-            {
-                cLine++; //contando as linhas
-                line = content; //recebendo o conteudo do html linha a linha
-                #region Contem / Não Contem
-                if (line.Contains(HTTP) && line.Contains(Custom) && line.Contains(HREF)) //verificando se a linha contem o http
-                {
-                    #region Primeira Verificação
-                    result1 = line;
-                    stLine = line.IndexOf(HREF);
-                    result1 = line.Substring(stLine);
-                    l2Line = line.IndexOf(Custom);
-                    fiLine = l2Line + 4;
-                    szLine = fiLine - stLine;
-                    result1 = line.Substring(stLine, szLine);
-                    #endregion
-
-                    #region Segunda Verificação
-                    result2 = result1;
-                    stLine = result1.LastIndexOf(HREF);
-                    result2 = result1.Substring(stLine);
-                    l2Line = result1.IndexOf(Custom);
-                    fiLine = l2Line + 4;
-                    szLine = fiLine - stLine;
-                    result2 = result1.Substring(stLine, szLine);
-                    #endregion
-
-                    #region Resultado
-                    line = result2;
-                    rtbResult.AppendText(cLine + " | " + line);
-                    rtbResult.AppendText("\n");
-                    #endregion
-                }
-                 
-
-                #endregion
+                htmlFile = @"c:\windows\temp\enem.html";
             }
+            String HREF = "href=";
+            #region Variaveis
+            String content;
+            String line;
+            String result1;
+            String result2;
+            int cLine = 0;
+            int stLine = 0;
+            int fiLine = 0;
+            int szLine = 0;
+            int l2Line = 0;
+            StreamReader str = new StreamReader(htmlFile);
             #endregion
+            #region Leitura do HTML
+            while ((content = str.ReadLine()) != null)
+            {
+                cLine++; //contando as linhas
+                line = content; //recebendo o conteudo do html linha a linha
+                #region Contem / Não Contem
+                if (line.Contains(TYPE) && line.Contains(EXT) && line.Contains(HREF)) //verificando se a linha contem o http
+                {
+                    #region Primeira Verificação
+                    result1 = line;
+                    stLine = line.IndexOf(TYPE);
+                    result1 = line.Substring(stLine);
+                    l2Line = line.IndexOf(EXT);
+                    fiLine = l2Line + EXT.Length;
+                    szLine = fiLine - stLine;
+                    result1 = line.Substring(stLine, szLine);
+                    #endregion
 
-            lcLines.Text = Convert.ToString(cLine);
+
+                    #region Segunda Verificação
+                    result2 = result1;
+                    stLine = result1.LastIndexOf(HREF);                 
+                    result2 = result1.Substring(stLine);
+                    l2Line = result1.IndexOf(EXT);
+                    fiLine = l2Line + EXT.Length;
+                    szLine = fiLine - stLine;
+                    result2 = result1.Substring(stLine, szLine);
+                    #endregion
+                  
+
+                    #region Resultado
+                    line = result2;
+                    rtbResult.AppendText(cLine + " | " + line);
+                    rtbResult.AppendText("\n");
+                    #endregion
+
+                }
+
+                lcLines.Text = Convert.ToString(cLine);
+            }
         }
+        #endregion
+        #endregion
+        #region Funções2
         public void Clear()
         {
             rtbResult.Text = " ";
@@ -250,66 +174,23 @@ namespace ExtraiPDF
             ofd.Title = "Selecionar Arquivo";
             ofd.InitialDirectory = @"c:\windows\temp";
             ofd.Filter = "HTML|*.html";
-            ofd.ShowDialog();
-
-        }
-        public void Checks()
-        {
-            if (checkBox1.Checked)
+            ofd.CheckFileExists = true;
+            ofd.CheckPathExists = true;
+            ofd.FilterIndex = 2;
+            ofd.RestoreDirectory = true;
+            ofd.ReadOnlyChecked = true;
+            ofd.ShowReadOnly = true;
+            DialogResult dr = ofd.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
             {
-                checkBox1.Text = "Checked";
+                foreach (String arquivo in ofd.FileNames)
+                {
+                    htmlFile = arquivo;
+                    PATH.Text = arquivo;
+                }
             }
-            else
-            {
-                checkBox1.Text = "Unchecked";
-            }
-            if (checkBox2.Checked)
-            {
-                checkBox2.Text = "Checked";
-            }
-            else
-            {
-                checkBox2.Text = "Unchecked";
-            }
-            if (checkBox3.Checked)
-            {
-                checkBox3.Text = "Checked";
-            }
-            else
-            {
-                checkBox3.Text = "Unchecked";
-            }
-            if (checkBox4.Checked)
-            {
-                checkBox4.Text = "Checked";
-            }
-            else
-            {
-                checkBox4.Text = "Unchecked";
-            }
-            if (checkBox5.Checked)
-            {
-                checkBox5.Text = "Checked";
-            }
-            else
-            {
-                checkBox5.Text = "Unchecked";
-            }
-            if (checkBox6.Checked)
-            {
-                checkBox6.Text = "Checked";
-            }
-            else
-            {
-                checkBox6.Text = "Unchecked";
-            }
-
-        }
-        #endregion
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Open();
+            #endregion
+            #endregion
         }
     }
 }
